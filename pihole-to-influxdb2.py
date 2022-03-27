e256a4b78c4f416b0f4eb15c854b58bf2de2a26d
gianni <gianni.costanzi@gmail.com>
2022-03-27 10:05:24 +0200

    Added error handling for API errors

--- pihole-to-influxdb2.py
+++ pihole-to-influxdb2.py
@@ -111,7 +111,13 @@ if __name__ == '__main__':
                 print(e, file=sys.stderr)
                 print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Connection Error: Could not connect to {host}:{host_port}({host_name})",file=sys.stderr)
                 continue
-
+            
+            # Check if API call returned an error
+            if "error" in stats:
+                error=stats.pop("error")
+                failure = True
+                print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] API Error: <{error}> for {host}:{host_port}({host_name})",file=sys.stderr)
+                continue
 
             gravity_last_updated = stats.pop("gravity_last_updated")
             gravity_file_exists = gravity_last_updated["file_exists"]
